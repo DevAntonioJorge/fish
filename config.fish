73420e90dbbda2fd3da2a8ce0744f1206438849f
Antonio <antoniofilhojsa@gmail.com>
2025-08-19 17:23:22 -0300

    add golang to path

--- config.fish
+++ config.fish
@@ -1,5 +1,6 @@
 if status is-interactive
     source (starship init fish --print-full-init | psub)
     set -gx PATH /home/aj/.cargo/bin $PATH
+    set -gx PATH /usr/local/go/bin $PATH
     zoxide init fish | source
 end
